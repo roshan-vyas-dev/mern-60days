a22cb3c8cb4b4dc6acd7000c80f29be46e3d981a
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-06-12 10:24:06 +0530

    Day 12 - ES6 modules import export

---
+++ js-foundation/day12-modules/greet.js
@@ -0,0 +1,12 @@
+// Named export
+export const greetUser = (name) => {
+    return `Hello ${name}! Welcome to Day 12.`;
+};
+
+export const greetMorning = (name) => {
+    return `Good morning ${name}!`;
+};
+
+// Default export
+const appName = "MERN Learning App";
+export default appName;
---
+++ js-foundation/day12-modules/math.js
@@ -0,0 +1,9 @@
+// Named exports - can have multiple
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
+export const divide = (a, b) => a / b;
+
+// Default export - only one per file
+const PI = 3.14159;
+export default PI;
